d6a5a6a29894fe38749326b622ce7215230d228b
Malak Abu Ramila <malakabur@edu.hac.ac.il>
2024-06-08 19:23:20 +0300

    initial commit

---
+++ src/main/java/hac/bla/BlaApplication.java
@@ -0,0 +1,13 @@
+package hac.bla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hac/bla/BlaApplicationTests.java
@@ -0,0 +1,13 @@
+package hac.bla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
